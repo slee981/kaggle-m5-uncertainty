218bb5150fcaec28cf82b77f572e60ce635879f8
Stephen Lee <slee@frbchi.org>
2020-04-05 16:48:31 -0500

    add weekday-dept correlation matrix

--- explore.R
+++ explore.R
@@ -115,3 +115,44 @@ dept_corr_mat %>%
     labs(
         title = "Correlation by Department"
     )
+
+#------------------------------------------------------------------------------
+# correlation by weekday-dept
+
+weekday_dept_data <- data %>% 
+    group_by(wm_yr_wk, weekday, dept_id) %>% 
+    summarise(
+        sales = mean(sales)
+    ) %>% 
+    unite(
+        "weekday_dept", weekday:dept_id
+    ) %>%
+    pivot_wider(
+        id_cols = wm_yr_wk, 
+        names_from = weekday_dept, 
+        values_from = sales
+    ) %>% 
+    ungroup() %>% 
+    select(- wm_yr_wk) 
+
+weekday_dept_corr_mat <- weekday_dept_data %>% 
+    as.data.frame() %>% 
+    cor(use = "pairwise.complete.obs")
+
+weekday_dept_corr_mat %>% 
+    ggcorrplot(
+        legend.title = "", 
+        outline.color = "white"
+    ) +
+    theme(
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        axis.text.x = element_blank(),
+        axis.text.y = element_blank(),
+        axis.ticks.x = element_blank(),
+        axis.ticks.y = element_blank(),
+        plot.title = element_text(hjust = 0.5)
+    ) +
+    labs(
+        title = "Correlation by weekday-department"
+    )
